aaa50bdf50f5b8b755e24dffc0f6fb97ba5de4f3
Luis Guillermo Pérez Rodríguez <luisguillermo.rodriguez32@gmail.com>
2025-03-03 08:21:26 -0500

    initial project

---
+++ app/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from contextlib import asynccontextmanager
+from app.database import init_db
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await init_db()
+    yield
+
+app = FastAPI(lifespan=lifespan)
+
+@app.get("/")
+async def root():
+    return {"message": "FastAPI CRUD Challenge"}
