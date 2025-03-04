75a8fab7f5221727b321215347f70ad08bfdd1f8
Luis Guillermo Pérez Rodríguez <luisguillermo.rodriguez32@gmail.com>
2025-03-03 23:12:29 -0500

    refactor authentication logic and move user retrieval to auth module

---
+++ app/auth.py
@@ -0,0 +1,37 @@
+from fastapi.security import OAuth2PasswordBearer
+from fastapi import Depends, HTTPException, status
+from typing import Annotated
+from jwt.exceptions import InvalidTokenError
+from sqlalchemy.orm import Session
+from app.env import SECRET_KEY, ALGORITHM
+import jwt
+from pydantic import BaseModel
+from app.database import User
+from sqlalchemy.sql import select
+
+class TokenData(BaseModel):
+    username: str | None = None
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+async def get_user(db: Session, username: str):
+    return (await db.execute(select(User).filter(User.username == username))).scalars().first()
+
+async def get_current_user(db: Session, token: Annotated[str, Depends(oauth2_scheme)]):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except InvalidTokenError:
+        raise credentials_exception
+    user = get_user(db, username=token_data.username)
+    if user is None:
+        raise credentials_exception
+    return user
--- app/routers/users.py
+++ app/routers/users.py
@@ -2,24 +2,19 @@ from datetime import datetime, timedelta, timezone
 from fastapi import APIRouter, HTTPException, status, Depends
 from pydantic import BaseModel
 
-from typing import Annotated
-
 
 import jwt
 from passlib.context import CryptContext
-from jwt.exceptions import InvalidTokenError
 
 from sqlalchemy.orm import Session
 from sqlalchemy.sql import select
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.database import User, get_session
 from app.env import SECRET_KEY, ALGORITHM, ACCESS_TOKEN_EXPIRE_MINUTES
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from app.auth import get_user
 
 
 router = APIRouter()
-
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 # Password hashing
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
@@ -27,10 +22,6 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 class Token(BaseModel):
     access_token: str
     token_type: str
-
-
-class TokenData(BaseModel):
-    username: str | None = None
 
 class BaseUser(BaseModel):
     username: str
@@ -53,28 +44,6 @@ def get_password_hash(password):
 
 def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
-
-async def get_user(db: Session, username: str):
-    return (await db.execute(select(User).filter(User.username == username))).scalars().first()
-
-async def get_current_user(db: Session, token: Annotated[str, Depends(oauth2_scheme)]):
-    credentials_exception = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Could not validate credentials",
-        headers={"WWW-Authenticate": "Bearer"},
-    )
-    try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        username = payload.get("sub")
-        if username is None:
-            raise credentials_exception
-        token_data = TokenData(username=username)
-    except InvalidTokenError:
-        raise credentials_exception
-    user = get_user(db, username=token_data.username)
-    if user is None:
-        raise credentials_exception
-    return user
 
 async def authenticate_user(db: Session, username: str, password: str):
     user = await get_user(db, username)
